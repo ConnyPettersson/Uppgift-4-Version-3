7fdf4b5ab8868e5ae6de4767043fe7494a5f4d60
PedalConny <pedalconny@hotmail.com>
2023-09-28 12:40:49 +0200

    added const variables in main js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 console.log("main.js is loaded...");
 
-const input
+const input = document.querySelector("#todoInput");
+const list = document.querySelector("ul");
+const button = document.querySelector("addTodo");
+const info = document.querySelector("small");
+const completed = document.querySelector("p");
+

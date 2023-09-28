5074578a92827d2fbf794676e3c0bec1b8faeb7d
PedalConny <pedalconny@hotmail.com>
2023-09-28 13:14:03 +0200

    added button listener and list appenchild

--- main.js
+++ main.js
@@ -2,7 +2,27 @@ console.log("main.js is loaded...");
 
 const input = document.querySelector("#todoInput");
 const list = document.querySelector("ul");
-const button = document.querySelector("addTodo");
+const button = document.querySelector("#addTodo");
 const info = document.querySelector("small");
-const completed = document.querySelector("p");
+const completedInfo = document.querySelector("p");
 
+let completedCount = 0;
+
+
+button.addEventListener("click", function () {
+
+    const text = input.value;
+
+    if (text.length == 0) {
+        info.innerText = "Input must not be empty";
+
+        return;
+    }
+    else {
+        info.innerText = "";
+    }
+
+    const item = document.createElement("li");
+    list.appendChild(item);
+
+})
